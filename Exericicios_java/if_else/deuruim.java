0078e5586d188639d79b806c56c16fb8d2bb707c
Gabriel Dazilio <gabriel.fanchiotti@hotmail.com>
2025-09-18 20:50:13 -0300

    acertar a data

---
+++ Exericicios_java/if_else/deuruim.java
@@ -0,0 +1,8 @@
+package Exericicios_java.if_else;
+
+public class deuruim {
+    public static void main(String[] args) {
+        
+        System.out.println("conta direito os dias github");
+    }
+}
