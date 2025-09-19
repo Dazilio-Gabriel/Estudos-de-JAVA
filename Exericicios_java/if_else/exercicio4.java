5091410fb4ddc13ef9bc7262da0bba1d08bf0a5c
Gabriel Dazilio <gabriel.fanchiotti@hotmail.com>
2025-09-18 23:49:13 -0300

    exercicio .equals

---
+++ Exericicios_java/if_else/exercicio4.java
@@ -0,0 +1,23 @@
+package Exericicios_java.if_else;
+
+import java.util.Scanner;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String senha = "SenhaSuperSecreta123@";
+
+        System.out.println("digite a senha pre-salva");
+        String senhaDoUsuario = scanner.nextLine();
+
+        if (senha.equals(senhaDoUsuario)) {
+            System.out.println("voce digitou a senha corretamente\n"
+                    + "Voce merece um chocolate");
+        } else {
+            System.out.println("voce digitou a senha errada, b u r r o");
+        }
+
+        scanner.close();
+    }
+}
