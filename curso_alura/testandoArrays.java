8385851f9bc396e5affcbc60c01b50eda97fe1c2
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-10-21 22:03:52 -0300

    iniciando estudos com vetores e array

---
+++ curso_alura/testandoArrays.java
@@ -0,0 +1,28 @@
+package curso_alura;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class testandoArrays {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite quantos numeros irao entrar no seu array");
+        int numero = scanner.nextInt();
+
+        int[] vetor = new int[numero];
+
+        for (int i = 0; i < numero; i++) {
+            System.out.println("digite o numero do vetor " + (i + 1) + " i");
+            int numerosVetor = scanner.nextInt();
+            vetor[i] = numerosVetor;
+
+        }
+
+        System.out.println(Arrays.toString(vetor));
+
+        scanner.close();
+    }
+}
---
+++ desafio_3Alura/Main.java
@@ -0,0 +1,11 @@
+package desafio_3Alura;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+
+        
+    }
+}
