de3fa431856620fe897271099a21c1a784c17ff2
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-11-25 18:19:37 -0300

    comecando a relembrar array list

---
+++ Exericicios_java/arrayList/ex1.java
@@ -0,0 +1,24 @@
+package Exericicios_java.arrayList;
+
+import java.util.ArrayList;
+
+/*Crie um ArrayList de Strings chamado nomes e:
+Adicione 5 nomes.
+Mostre todos os nomes na tela.  */
+
+public class ex1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> nomes = new ArrayList<String>();
+
+        nomes.add("joao");
+        nomes.add("carlos");
+        nomes.add("adriano");
+        nomes.add("Ana Elisa");
+        nomes.add("Gabriel");
+
+        for (String n : nomes) {
+            System.out.println("[" + n + "]");
+        }
+    }
+}
