254457579be60e7869a17c7434d6af17dba207ce
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-11-25 18:28:51 -0300

    ex2 ArrayList~

---
+++ Exericicios_java/arrayList/ex2.java
@@ -0,0 +1,26 @@
+package Exericicios_java.arrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* Crie um ArrayList de Integer e peça ao usuário 5 números.
+Depois, exiba todos eles.*/
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("digite o numero 5 numeros inteiros");
+            int controle = scanner.nextInt();
+            numeros.add(controle);
+        }
+
+        for (int n : numeros) {
+            System.out.println("[" + n + "]");
+        }
+
+    }
+}
